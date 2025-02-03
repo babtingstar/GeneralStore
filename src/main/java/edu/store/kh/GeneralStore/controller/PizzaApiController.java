87999f13da456fd9cf3161881171ed91cf56d12e
babtingstar <babtingstar@naver.com>
2025-02-03 15:47:03 +0900

    pizza api update

--- src/main/java/edu/store/kh/GeneralStore/controller/PizzaApiController.java
+++ src/main/java/edu/store/kh/GeneralStore/controller/PizzaApiController.java
@@ -5,6 +5,7 @@ import edu.store.kh.GeneralStore.service.PizzaService;
 import edu.store.kh.GeneralStore.service.PizzaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -25,14 +26,28 @@ public class PizzaApiController {
     public void createPizza() {
         return ;
     }
-
+    
     @GetMapping("/pizzas/{id}")
     public Pizza getPizzaById(@PathVariable int id) {
         return pizzaService.selectById(id);
     }
-
+    
+    // @RequestBody -> @RequestParam 변경 @MultipartFile 추가
     @PutMapping("/pizzas/{id}")
-    public void updatePizza() {}
+    public void updatePizza(@PathVariable int id,
+                            @RequestPart("name") String name,
+                            @RequestPart("price") int price,
+                            @RequestPart("description") String description,
+                            @RequestPart("imagePath") MultipartFile file) {
+        Pizza pizza = new Pizza();
+        pizza.setId(id);
+        pizza.setName(name);
+        pizza.setPrice(price);
+        pizza.setDescription(description);
+        String filePath = file.
+        pizza.setImagePath(file.get);
+        pizzaService.updatePizza(pizza);
+    }
 
     @DeleteMapping("/pizzas/{id}")
     public void deletePizza(@PathVariable int id) {
