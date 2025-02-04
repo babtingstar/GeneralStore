4fff1e28a1dce7a30247318e0a76114c9aa2f33e
babtingstar <babtingstar@naver.com>
2025-02-04 12:34:00 +0900

    pizza pai insert update uuid currentTimeMills

--- src/main/java/edu/store/kh/GeneralStore/controller/PizzaApiController.java
+++ src/main/java/edu/store/kh/GeneralStore/controller/PizzaApiController.java
@@ -1,7 +1,6 @@
 package edu.store.kh.GeneralStore.controller;
 
 import edu.store.kh.GeneralStore.dto.Pizza;
-import edu.store.kh.GeneralStore.service.PizzaService;
 import edu.store.kh.GeneralStore.service.PizzaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,8 +22,12 @@ public class PizzaApiController {
     }
 
     @PostMapping("/pizzas")
-    public void createPizza() {
-        return ;
+    public void createPizza(@PathVariable int id,
+                            @RequestParam("name") String name,
+                            @RequestParam("price") int price,
+                            @RequestParam("description") String description,
+                            @RequestParam(value = "imagePath", required = false) MultipartFile imagePath) {
+        pizzaService.insertPizza(id, name, price, description, imagePath);
     }
     
     @GetMapping("/pizzas/{id}")
@@ -33,20 +36,24 @@ public class PizzaApiController {
     }
     
     // @RequestBody -> @RequestParam 변경 @MultipartFile 추가
+    // @RequestPart @RequestParam
     @PutMapping("/pizzas/{id}")
     public void updatePizza(@PathVariable int id,
-                            @RequestPart("name") String name,
-                            @RequestPart("price") int price,
-                            @RequestPart("description") String description,
-                            @RequestPart("imagePath") MultipartFile file) {
-        Pizza pizza = new Pizza();
-        pizza.setId(id);
-        pizza.setName(name);
-        pizza.setPrice(price);
-        pizza.setDescription(description);
-        String filePath = file.
-        pizza.setImagePath(file.get);
-        pizzaService.updatePizza(pizza);
+                            @RequestParam("name") String name,
+                            @RequestParam("price") int price,
+                            @RequestParam("description") String description,
+                            @RequestParam(value = "imagePath", required = false) MultipartFile imagePath) {
+                            // required = false @RequestParam 요청으로 프론트엔드에서 값을 가져올 때 null 값이여도 가능
+                            // @RequestParam required : default = true
+                            // DB 에서 not null 일 경우 required 를 true 로 줌 / null 일 경우 required = false 작성해 줌
+
+        /* Controller 는 주로 Backend ↔ Frontend 중간 다리 역할
+           똑같은 URL API 주소에서는 GET POST PUT DELETE 최초 1회씩만 사용 가능 (각 선착순)
+
+           Controller 에서는 Frontend 에서 가져온 데이터를 service 로 전달만 함
+           또는 DB 에서 Backend 에서 가져온 데이터 @Service 로 전달
+        */
+        pizzaService.updatePizza(id, name, price, description, imagePath);
     }
 
     @DeleteMapping("/pizzas/{id}")
